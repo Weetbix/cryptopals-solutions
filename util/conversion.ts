c7be1c796d465d7e055b9ef9fecc2dabfc54e21e
John Hannagan <weetbix_@hotmail.com>
2019-03-11 23:09:05 +0100

    Solution for #5

--- util/conversion.ts
+++ util/conversion.ts
@@ -2,6 +2,17 @@ export function binaryToAscii(binary: Uint8Array): string {
   return new TextDecoder("utf-8").decode(binary);
 }
 
+export function binaryToHex(binary: Uint8Array) {
+  return binary.reduce(
+    (hex, byte) =>
+      hex +
+      Number(byte)
+        .toString(16)
+        .padStart(2, "0"),
+    ""
+  );
+}
+
 export function hexToBinary(hex: string) {
   const buffer = new Uint8Array(hex.length / 2);
 
