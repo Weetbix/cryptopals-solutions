efb78d9443c220c200c4114df8949274ac4808be
John Hannagan <weetbix_@hotmail.com>
2019-05-14 23:06:27 +0200

    Add hamming distance implementation

---
+++ 6-break-repeating-key-XOR/index.ts
@@ -0,0 +1,3 @@
+import { hammingDistance } from "../util/string.ts";
+
+console.log(hammingDistance("this is a test", "wokka wokka!!!"));
--- util/string.ts
+++ util/string.ts
@@ -4,3 +4,28 @@ export function scoreText(text: string) {
     0
   );
 }
+
+/**
+ * Returns the number of different bits in string a vs string b
+ * ie the hamming or edit distance
+ */
+export function hammingDistance(a: string, b: string): number {
+  const encoder = new TextEncoder();
+  return hammingDistanceBytes(encoder.encode(a), encoder.encode(b));
+}
+
+function hammingDistanceBytes(a: Uint8Array, b: Uint8Array): number {
+  return a.reduce((acc, current, i) => {
+    // First get a variable holding all the different bits between the two
+    let difference = a[i] ^ b[i];
+    let bitCount = 0;
+    // Iterate over bit of the difference and count the bits that are 1
+    while (difference !== 0) {
+      // Add the least significant bit
+      bitCount += difference & 1;
+      // set next as least significant bit
+      difference = difference >> 1;
+    }
+    return acc + bitCount;
+  }, 0);
+}
