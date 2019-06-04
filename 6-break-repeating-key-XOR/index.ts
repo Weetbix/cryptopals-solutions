c8d20e02ebadd8af720922c4cdaa528689aa22a7
John Hannagan <weetbix_@hotmail.com>
2019-06-04 22:45:54 +0200

    Complete ex 6

--- 6-break-repeating-key-XOR/index.ts
+++ 6-break-repeating-key-XOR/index.ts
@@ -1,8 +1,11 @@
-import { base64ToUint8Array } from "../util/conversion.ts";
-import { attemptKeyGuess } from "../util/repeatingKeyXOR.ts";
+import { base64ToUint8Array, binaryToAscii } from "../util/conversion.ts";
+import { attemptKeyGuess, encrypt } from "../util/repeatingKeyXOR.ts";
 
 const decoder = new TextDecoder("utf-8");
 const base64Input = decoder.decode(Deno.readFileSync("./input.txt"));
 const binaryInput = base64ToUint8Array(base64Input);
 
-console.log(attemptKeyGuess(binaryInput));
+const keyGuess = attemptKeyGuess(binaryInput);
+console.log("Key Guess: " + keyGuess);
+console.log("Decryption: ");
+console.log(binaryToAscii(encrypt(keyGuess, binaryInput)));
--- util/repeatingKeyXOR.ts
+++ util/repeatingKeyXOR.ts
@@ -15,7 +15,13 @@ export interface KeysizeHamming {
  * The KEYSIZE with the smallest normalized edit distance is probably the key.
  */
 export function keysizeRankings(data: Uint8Array): KeysizeHamming[] {
-  const KEYSIZE_MIN_BYTES = 2;
+  // The cryptopals challenges say take the key sizes
+  // from 2 to 40, but here I set the lower limit to 4.
+  // Otherwise the keys with 2 and 3 bytes have a lower
+  // hamming distance and are falsely ranked higher.
+  // This is even after applying them "Take 4 keysize blocks
+  // instead of 2 and average the distance" suggestion
+  const KEYSIZE_MIN_BYTES = 4;
   const KEYSIZE_MAX_BYTES = 40;
 
   let results = [];
@@ -25,23 +31,41 @@ export function keysizeRankings(data: Uint8Array): KeysizeHamming[] {
     keysize <= KEYSIZE_MAX_BYTES;
     keysize++
   ) {
+    // we are going to take 4 KEY_SIZE blocks and average the hamming distance between them
     const first = data.slice(0, keysize);
     const second = data.slice(keysize, keysize * 2);
+    const third = data.slice(keysize * 2, keysize * 3);
+    const fourth = data.slice(keysize * 3, keysize * 4);
+
+    const dist =
+      (hammingDistanceBytes(first, second) +
+        hammingDistanceBytes(second, third) +
+        hammingDistanceBytes(third, fourth)) /
+      3.0;
 
     results.push({
       keysize,
-      distance: hammingDistanceBytes(first, second) / keysize
+      distance: dist / keysize
     });
   }
 
   return results.sort((a, b) => a.distance - b.distance);
 }
 
+/**
+ * Given an input which is binary data encrypted by some
+ * repeating key XOR, this function returns a string that
+ * is likely the key.
+ *
+ * @param data Binary data encrypted by repeating key XOR
+ */
 export function attemptKeyGuess(data: Uint8Array): string {
   // Determine the likely key size
   const keysizeGuesses = keysizeRankings(data);
-  console.log(keysizeGuesses);
-  const keysize = keysizeGuesses[2].keysize;
+
+  // If our keysize rankings is working correctly it should
+  // be the keysize with the highest rank
+  const keysize = keysizeGuesses[0].keysize;
 
   // split the input into KEY SIZE sized chunks
   const chunked = chunk(data, keysize);
@@ -50,8 +74,6 @@ export function attemptKeyGuess(data: Uint8Array): string {
   // are encrypted using the same single XOR key
   const byteAlignedArrays = tranpose(chunked);
 
-  console.log(byteAlignedArrays.length);
-
   return byteAlignedArrays
     .map(singleByteXORData => {
       const keyByte = getBestDecryption(singleByteXORData).key;
@@ -59,4 +81,14 @@ export function attemptKeyGuess(data: Uint8Array): string {
       return keyAscii;
     })
     .join("");
+}
+
+/**
+ * Applies repeated key XOR encryption/decryption on a given data array
+ */
+export function encrypt(key: string, data: Uint8Array): Uint8Array {
+  const encoder = new TextEncoder();
+  const keyData = encoder.encode(key);
+
+  return data.map((byte, index) => byte ^ keyData[index % key.length]);
 }
